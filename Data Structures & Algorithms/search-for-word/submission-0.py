bd9d2362f169f9daf3a178253494d1a19e054405
Khamidullokhon <121408143+KhamidullokhonA@users.noreply.github.com>
2026-04-11 17:26:03 -0400

    Add: search-for-word - submission-0

---
+++ Data Structures & Algorithms/search-for-word/submission-0.py
@@ -0,0 +1,32 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        row, col = len(board), len(board[0])
+
+        visited = set()
+
+        def backtrack(r, c, i):
+            if i == len(word):
+                return True
+
+            if r>=row or c>=col or r<0 or c<0 or board[r][c]!=word[i] or (r,c) in visited:
+                return False
+
+            visited.add((r,c))
+            found = (backtrack(r+1, c, i+1) or 
+            backtrack(r-1, c, i+1) or 
+            backtrack(r, c+1, i+1) or 
+            backtrack(r, c-1, i+1)) 
+            visited.remove((r,c))
+
+            return found
+
+        for i in range(row):
+            for j in range(col):
+                if backtrack(i, j, 0):
+                    return True
+
+
+        return False
+        
+
+    
