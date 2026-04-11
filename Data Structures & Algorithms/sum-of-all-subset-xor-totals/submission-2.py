28b70097ba8a0bd80b6f439fe45b534f8cf52924
Khamidullokhon <121408143+KhamidullokhonA@users.noreply.github.com>
2026-04-11 14:58:18 -0400

    Add: sum-of-all-subset-xor-totals - submission-2

---
+++ Data Structures & Algorithms/sum-of-all-subset-xor-totals/submission-2.py
@@ -0,0 +1,22 @@
+class Solution:
+    def subsetXORSum(self, nums: List[int]) -> int:
+         
+        total = 0
+
+        def backtrack(start, path):
+            nonlocal total
+            
+            xorr = 0
+
+            for i in range(start, len(nums)):
+
+                path.append(nums[i])
+                backtrack(i+1, path)
+                path.pop()
+
+            for each in path:
+                xorr ^= each 
+            total+=xorr
+
+        backtrack(0, [])
+        return total
